95ea7a4f3e67220ecf1e712fb3820dc3f6d422f7
Talmage <talmagelucas@gmail.com>
2020-10-24 12:52:44 -0600

    nothing new really

--- commands/draft.js
+++ commands/draft.js
@@ -13,10 +13,36 @@ module.exports = {
         let draft = con.query("SELECT * FROM lobby ORDER BY elo DESC LIMIT 2", function (err, rows, fields) {
             if (err) throw err
             if (message.author.tag == rows[0].tag || message.author.tag == rows[1].tag) {
-                console.log(message.mentions)
+                if (args[0]) {
+                    const user = getUserFromMention(args[0]);
+                    if (!user) {
+                        console.log(user)
+                        return message.reply('Please mention a player to draft them.');
+                    }
+
+                    con.query("UPDATE lobby SET team = ? WHERE tag = ?", "red", `${user.tag}`, (error, data) => {
+                        if (error) throw error
+                    })
+                }
+
+
+
             }
 
         })
+        function getUserFromMention(mention) {
+            if (!mention) return;
+
+            if (mention.startsWith('<@') && mention.endsWith('>')) {
+                mention = mention.slice(2, -1);
+
+                if (mention.startsWith('!')) {
+                    mention = mention.slice(1);
+                }
+
+                return Client.users.cache.get(mention);
+            }
+        }
     },
 };
 
